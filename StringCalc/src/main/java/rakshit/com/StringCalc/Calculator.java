2e71b68502e36093a0c640e580bc855c472faba5
Lenovo <Lenovo@DESKTOP-PP9F7BC>
2021-02-25 18:17:00 +0530

    Comments added

--- StringCalc/src/main/java/rakshit/com/StringCalc/Calculator.java
+++ StringCalc/src/main/java/rakshit/com/StringCalc/Calculator.java
@@ -3,32 +3,36 @@ package rakshit.com.StringCalc;
 import java.util.HashSet;
 
 public class Calculator {
-	HashSet<String> del= new HashSet<String>();
+	HashSet<String> del= new HashSet<String>();//To save multiple Delimiters
+	
 	public int add(String s) throws Exception {
-		   String deli="[,|\n";
+		   String deli="[,|\n";//Standard delimiters
 		 
-		  if(s.length()==0)return 0;
+		  if(s.length()==0)return 0;//Empty string
+		  
+		  //Multiple and multicharacter delimiters
 		  if(s.charAt(0)=='/' && s.charAt(2)=='[') {
-				 s=findMultiDeli(s);
-				 return getSum2(s);
+				 s=findMultiDeli(s);//function to save delimiters to hash set
+				 return getSum2(s);//function to get sum
 			 }
+		  //Different delimiter
 		  else if(s.charAt(0)=='/') {
-			  deli+=s.charAt(2);
+			  deli+=s.charAt(2);//adding different delimiter
 			  s = s.replace(s.substring(0,4), "");
 		  }
 		  deli+=']';
-		  String[] numbers =s.split(deli);
-		  if(numbers.length==1)return Integer.parseInt(numbers[0]);
+		  String[] numbers =s.split(deli);//splitting using delimiters
+		  if(numbers.length==1)return Integer.parseInt(numbers[0]);//Contains only one number case
 		  
 		  return getSum(numbers);
 }
 	
-	
+	//Function to get sum
 	public int getSum(String[] numbers)throws Exception{
 		int sum=0;
-		  fireNegative(numbers);
+		  fireNegative(numbers);//negative numbers erroor
 		  for(String temp:numbers) {
-			  if(Integer.parseInt(temp)>1000)continue;
+			  if(Integer.parseInt(temp)>1000)continue;//more than thousand error
 			  sum+=Integer.parseInt(temp);
 		  }
 		  
@@ -43,6 +47,7 @@ public class Calculator {
 		  }
 	  }
 	
+	//function to find and save delimiters
 	public String findMultiDeli(String s) {
 		  int l=0,h=0,n=s.length();
 		  while(l<n && h<n) {
@@ -58,7 +63,7 @@ public class Calculator {
 		  s = s.replace(s.substring(0,h+1), "");
 		  return s;
 	  }
-	
+	//function to check valid delimiters and get sum
 	  public int getSum2(String s) throws Exception {
 		  int l=0,h=0,n=s.length(),sum=0;
 		  
@@ -67,13 +72,15 @@ public class Calculator {
 		         if(Character.isDigit(s.charAt(h))){
 		             while(h<n && (Character.isDigit(s.charAt(h))  || s.charAt(h)=='-')){h++;}
 
-		            if( Integer.parseInt(s.substring(l,h))<0)
-		            {
-		            	throw new Exception("negatives not allowed");
+			            if( Integer.parseInt(s.substring(l,h))<0)
+			            		throw new Exception("negatives not allowed");
+			      
+			            if( Integer.parseInt(s.substring(l,h))>1000)continue;
+			
+			             sum+= Integer.parseInt(s.substring(l,h));
+			             l=h;
 		            }
-		             sum+= Integer.parseInt(s.substring(l,h));
-		             l=h;
-		            }else{
+		         else{
 		             while(h<n && !Character.isDigit(s.charAt(h))){h++;}
 		             if(!del.contains(s.substring(l,h)))break;
 		             l=h;
