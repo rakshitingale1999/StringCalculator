1f5d9c9d030373e78cd6fc59f2be788968faf574
Lenovo <Lenovo@DESKTOP-PP9F7BC>
2021-02-25 16:52:27 +0530

    Created First Case for Empty String

--- StringCalc/src/main/java/rakshit/com/StringCalc/Calculator.java
+++ StringCalc/src/main/java/rakshit/com/StringCalc/Calculator.java
@@ -1,5 +1,11 @@
 package rakshit.com.StringCalc;
 
 public class Calculator {
-
+	public int add(String s) {
+		 
+		 
+		  if(s.length()==0)return 0;
+		 
+		  return 0;
+}
 }
--- StringCalc/src/main/java/rakshit/com/StringCalc/Tests.java
+++ StringCalc/src/main/java/rakshit/com/StringCalc/Tests.java
@@ -1,5 +1,13 @@
 package rakshit.com.StringCalc;
 
-public class Tests {
+import org.junit.Test;
+import org.junit.Assert;
 
+public class Tests {
+	Calculator c=new Calculator();
+	@Test
+	public void EmptyStringTest(){
+	    int n=c.add("");
+		Assert.assertEquals(0,n);
+	}
 }
