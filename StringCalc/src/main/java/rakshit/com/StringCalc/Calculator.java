5290c606d6a5a14dd345ca7a3904d99b574f92ae
Lenovo <Lenovo@DESKTOP-PP9F7BC>
2021-02-25 17:01:32 +0530

    Added numbers  with "," delimiter

--- StringCalc/src/main/java/rakshit/com/StringCalc/Calculator.java
+++ StringCalc/src/main/java/rakshit/com/StringCalc/Calculator.java
@@ -5,7 +5,16 @@ public class Calculator {
 		 
 		 
 		  if(s.length()==0)return 0;
-		 
-		  return 0;
+		  String[] numbers =s.split(",");
+		  if(numbers.length==1)return Integer.parseInt(numbers[0]);
+		  
+		  return getSum(numbers);
 }
+	 public int getSum(String[] numbers){
+		  int sum=0;
+		  for(String temp:numbers) {
+			  sum+=Integer.parseInt(temp);
+		  }
+		  return sum;
+	  }
 }
--- StringCalc/src/main/java/rakshit/com/StringCalc/Tests.java
+++ StringCalc/src/main/java/rakshit/com/StringCalc/Tests.java
@@ -10,4 +10,14 @@ public class Tests {
 	    int n=c.add("");
 		Assert.assertEquals(0,n);
 	}
+	@Test
+	public void StringWithOneNumberTest()  {
+	    int n=c.add("100");
+		Assert.assertEquals(100,n);
+	}
+	@Test
+	public void StringWithCommaNumbersTest() {
+	    int n=c.add("100,200");
+		Assert.assertEquals(300,n);
+	}
 }
